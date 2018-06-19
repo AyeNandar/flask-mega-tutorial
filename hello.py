3ab646b0a3798d47ae8ee8ccee2d0173f57d03aa
AyeNandar <andhandh1381@gmail.com>
2018-06-18 21:59:59 -0700

    Update my files

--- hello.py
+++ hello.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app=Flask(__name__)
-app.route("/")
+@app.route("/")
 def home():
     return "Hello World"
-app.route("/index")
+@app.route("/index")
 def index():
     return "This is index"
 if __name__=="__main__":
